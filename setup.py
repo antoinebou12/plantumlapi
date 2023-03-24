fefb6861795c2b2312da1634c00e3906f0e11b58
Antoine Boucher <antoine.bou13@gmail.com>
2023-03-24 10:55:42 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+from plantuml import __author__, __version_string__, __email__
+
+setup(
+    name='plantuml',
+    version=__version_string__,
+    description='',
+    long_description=open('README.md', 'r').read(),
+    url='https://github.com/antoinebou12/py-plantuml/',
+    author=__author__,
+    author_email=__email__,
+    license='BSD',
+    py_modules=['plantuml'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Utilities',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    install_requires=['httplib2', "httpx", "typer"],
+    keywords=['plantuml', 'uml']
+)
