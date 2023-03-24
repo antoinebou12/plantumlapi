50bfdfa511f858e9dba90d95a88cd193c3a180a3
Antoine Boucher <antoine.bou13@gmail.com>
2023-03-24 12:00:53 -0400

    Update plantuml.py

--- plantuml.py
+++ plantuml.py
@@ -1,17 +1,10 @@
-import base64
 from os import makedirs, path
-import string
-from argparse import ArgumentParser
 from io import open
 from typing import Optional
 from zlib import compress
-import contextlib
-from urllib.parse import urlencode, urlparse
 import httpx
 
 import typer
-
-from concept import generate_plantuml_script
 
 app = typer.Typer()
 
@@ -53,19 +46,6 @@ class PlantUMLHTTPError(PlantUMLConnectionError):
         if not getattr(self, 'message', None):
             self.message = message
         super().__init__(message, *args, **kwdargs)
-
-
-def deflate_and_encode(plantuml_text):
-    """zlib compress the plantuml text and encode it for the plantuml server.
-    """
-    plantuml_alphabet = string.digits + string.ascii_uppercase + string.ascii_lowercase + '-_'
-    base64_alphabet   = string.ascii_uppercase + string.ascii_lowercase + string.digits + '+/'
-    b64_to_plantuml = str.maketrans(base64_alphabet, plantuml_alphabet)
-    zlibbed_str = compress(plantuml_text.encode('utf-8'))
-    compressed_string = zlibbed_str[2:-4]
-    return base64.b64encode(compressed_string).translate(b64_to_plantuml).decode('utf-8')
-
-
 
 class PlantUML:
     """Connection to a PlantUML server with optional authentication.
@@ -110,15 +90,7 @@ class PlantUML:
         self.auth_type = 'basic_auth' if basic_auth else ('form_auth' if form_auth else None)
         self.auth = basic_auth or form_auth or None
 
-        # Proxify
-        with contextlib.suppress(ImportError):
-            if proxy_uri := httpx.ProxyURL(
-                url=environ.get('HTTPS_PROXY', environ.get('HTTP_PROXY'))
-            ):
-                http_opts.update(proxies=proxy_uri)
-                self.request_opts.update(proxies=proxy_uri)
-
-        self.client = httpx.Client(**http_opts)
+        self.client = httpx.Client(**http_opts, proxies=http_opts.get('proxies', None))
 
         if self.auth_type == 'basic_auth':
             self.client.auth = (self.auth['username'], self.auth['password'])
@@ -143,7 +115,7 @@ class PlantUML:
             except httpx.HTTPError as e:
                 raise PlantUMLConnectionError(e) from e
             if response.status_code != 200:
-                raise PlantUMLHTTPError(response)
+                raise PlantUMLHTTPError(response, "Login failed. Check your form_auth settings.")
             self.request_opts['Cookie'] = response.cookies.get_dict()
 
     def get_url(self, plantuml_text):
@@ -153,7 +125,7 @@ class PlantUML:
         :param str plantuml_text: The plantuml markup to render
         :returns: the plantuml server image URL
         """
-        return self.url + deflate_and_encode(plantuml_text)
+        return self.url + self.deflate_and_encode(plantuml_text)
 
     def processes(self, plantuml_text):
         """Processes the plantuml text into the raw PNG image data.
@@ -165,7 +137,7 @@ class PlantUML:
             response = httpx.get(url)
             response.raise_for_status()
         except httpx.HTTPError as e:
-            raise PlantUMLHTTPError(e) from e
+            raise PlantUMLHTTPError(e, "") from e
         return response.content
 
     def processes_file(self, filename, outfile=None, errorfile=None, directory=''):
@@ -201,12 +173,97 @@ class PlantUML:
         return True
 
 
-    @app.command()
+    def deflate_and_encode(self, plantuml_text):
+        """zlib compress the plantuml text and encode it for the plantuml server.
+        """
+        zlibbed_str = compress(plantuml_text.encode('utf-8'))
+        compressed_string = zlibbed_str[2:-4]
+        return self.encode(compressed_string)
+
+
+    def encode(self, data):
+        """encode the plantuml data which may be compresses in the proper
+        encoding for the plantuml server
+        """
+        res = ""
+        for i in range(0, len(self), 3):
+            if i + 2 == len(self):
+                res += self._encode3bytes(self[i], self[i + 1], 0)
+            elif i + 1 == len(self):
+                res += self._encode3bytes(self[i], 0, 0)
+            else:
+                res += self._encode3bytes(self[i], self[i + 1], self[i + 2])
+        return res
+
+
+    def _encode3bytes(self, b1, b2, b3):
+        c1 = b1 >> 2
+        c2 = ((b1 & 0x3) << 4) | (b2 >> 4)
+        c3 = ((b2 & 0xF) << 2) | (b3 >> 6)
+        c4 = b3 & 0x3F
+        res = ""
+        res += self._encode6bit(c1 & 0x3F)
+        res += self._encode6bit(c2 & 0x3F)
+        res += self._encode6bit(c3 & 0x3F)
+        res += self._encode6bit(c4 & 0x3F)
+        return res
+
+
+    def _encode6bit(self, b):
+        if b < 10:
+            return chr(48 + b)
+        b -= 10
+        if b < 26:
+            return chr(65 + b)
+        b -= 26
+        if b < 26:
+            return chr(97 + b)
+        b -= 26
+        if b == 0:
+            return '-'
+        return '_' if b == 1 else '?'
+
+    @app.command(
+        help='Generate images from plantuml defined files using plantuml server'
+    )
     def generate_images(files: list[str], out: str = '', server: str = 'http://www.plantuml.com/plantuml/img/'):
         """Generate images from plantuml defined files using plantuml server"""
-        pl = PlantUML(server)
+        pl = PlantUML(
+            url=server,
+            http_opts={'timeout': 60},
+            request_opts={'User-Agent': 'plantuml-markdown/0.1.0'}
+        )
         results = []
         for filename in files:
             gen_success = pl.processes_file(filename, directory=out)
             results.append({'filename': filename, 'gen_success': gen_success})
+
         typer.echo(results)
+
+
+    @app.command(
+        help='Generate images from plantuml defined files using plantuml server'
+    )
+    def generate_image(
+        file: str,
+        out: str = '',
+        server: str = 'http://www.plantuml.com/plantuml/img/',
+        auth: dict = None
+    ):
+        """Generate images from plantuml defined files using plantuml server"""
+        pl = PlantUML(
+            url=server,
+            http_opts={'timeout': 60},
+            request_opts={'User-Agent': 'plantuml-markdown/0.1.0'},
+            auth=auth
+        )
+        gen_success = pl.processes_file(file, directory=out)
+        typer.echo(gen_success
+    )
+
+def main():
+    app()
+
+
+if __name__ == '__main__':
+    main()
